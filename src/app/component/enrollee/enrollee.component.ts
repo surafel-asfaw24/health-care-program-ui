96da5033943bb6a0dd6b71d34922927798dcfd87
Surafel Asfaw <surafel.asfaw24@gmail.com>
2021-02-28 13:11:51 -0500

    creates basic components and services

---
+++ src/app/component/enrollee/enrollee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enrollee',
+  templateUrl: './enrollee.component.html',
+  styleUrls: ['./enrollee.component.scss']
+})
+export class EnrolleeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
