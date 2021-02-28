742bc693ed764f1105e94a788a2b9614e270efac
Surafel Asfaw <surafel.asfaw24@gmail.com>
2021-02-28 17:25:12 -0500

    connects components to service, fixes CORS issue

--- src/app/component/enrollees/enrollees.component.ts
+++ src/app/component/enrollees/enrollees.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {EnrolleeService} from '../../service/enrollee.service';
+import {Enrollee} from '../../model/enrollee.model';
 
 @Component({
   selector: 'app-enrollees',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enrollees.component.scss']
 })
 export class EnrolleesComponent implements OnInit {
+  enrollees: Enrollee[] = [];
 
-  constructor() { }
+  constructor(private enrolleeService: EnrolleeService) { }
 
   ngOnInit(): void {
+    this.fetchEnrollees();
   }
 
+  fetchEnrollees(): void {
+    this.enrolleeService.getEnrollees().subscribe(
+      value => {
+        this.enrollees = value;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/service/enrollee.service.ts
+++ src/app/service/enrollee.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Enrollee} from '../model/enrollee.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnrolleeService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getEnrollees(): Observable<Enrollee[]> {
+    return this.httpClient.get<Enrollee[]>('http://localhost:4200/api/enrollees');
+  }
+
+  getEnrollee(id: string): Observable<Enrollee> {
+    return this.httpClient.get<Enrollee>('http://localhost:4200/api/enrollees/' + id);
+  }
 }
